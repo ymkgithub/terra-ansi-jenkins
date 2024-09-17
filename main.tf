5b35444264c74af0d725b8aff9807ce74f18f8e4
maheshkumar <maheshkumar@bicsglobal.com>
2024-09-17 17:00:11 +0530

    uncommented the iam role conf for to ecr login

--- main.tf
+++ main.tf
@@ -17,28 +17,28 @@ provider "aws" {
   region = local.region
 }
 
-# resource "aws_iam_role" "aws_access" {
-#   name = "${var.prefix}-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-#   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
-# }
+resource "aws_iam_role" "aws_access" {
+  name = "${var.prefix}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
+}
 
-# resource "aws_iam_instance_profile" "instance_profile" {
-#   name = "${var.prefix}-profile"
-#   role = aws_iam_role.aws_access.name
-# }
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.prefix}-profile"
+  role = aws_iam_role.aws_access.name
+}
 
 # Get the latest Amazon Linux 2 AMI with gp2
 data "aws_ami" "amazon_linux_free_tier" {
@@ -63,7 +63,7 @@ resource "aws_instance" "managed_nodes" {
   instance_type = "t2.micro"
   key_name = var.ssh_key_name
   vpc_security_group_ids = [aws_security_group.sg.id]
-  # iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name        = "${element(var.names, count.index)}"
     stack       = var.prefix
